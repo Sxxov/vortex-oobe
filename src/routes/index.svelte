febe0b4d3ccba187bbe0d37e9894a265ee86cf18
jason lim <50891910+Sxxov@users.noreply.github.com>
2022-05-09 05:50:25 +0800

    Fix layout on mobile

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,17 +1,25 @@
 <script lang="ts">
 	import GameRenderer from '../components/game/GameRenderer.svelte';
+
+	let innerHeight: number;
 </script>
 
-<div type="/index" class="component">
+<svelte:window bind:innerHeight />
+
+<div type="/index" class="component" style="--height: {innerHeight}px">
 	<GameRenderer />
 </div>
 
 <style lang="postcss">
+	:global(body) {
+		overscroll-behavior-y: contain;
+	}
+
 	.component {
 		padding: 0;
 		box-sizing: border-box;
 		display: grid;
-		height: 100vh;
+		height: var(--height);
 		justify-items: center;
 		align-items: center;
 	}
