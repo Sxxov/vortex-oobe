70499a67f26b2d4a236d7f714042343b948be913
jason lim <50891910+Sxxov@users.noreply.github.com>
2022-06-21 23:39:27 +0800

    Bump `typescript`

--- svelte.config.js
+++ svelte.config.js
@@ -3,7 +3,6 @@ import rollupConfig from './rollup.config.js';
 import netlify from '@sveltejs/adapter-netlify';
 
 const production = process.env.NODE_ENV === 'production';
-const nameToHashes = new Map();
 
 /** @type {import('@sveltejs/kit').Config} */
 export default {
@@ -17,31 +16,6 @@ export default {
 	compilerOptions: {
 		// enable run-time checks when not in production
 		dev: !production,
-
-		cssHash: ({ hash: hasher, css, name }) => {
-			const hash = hasher(css);
-
-			if (production) {
-				return `_${hash}`;
-			}
-
-			let hashes = nameToHashes.get(name);
-
-			if (hashes == null) {
-				/** @type {string[]} */
-				const arr = [];
-				nameToHashes.set(name, arr);
-				hashes = arr;
-			}
-
-			let index = hashes.indexOf(hash);
-
-			if (index === -1) {
-				index = hashes.push(hash) - 1;
-			}
-
-			return `<${name}${index ? `-${index}` : ''}>`;
-		},
 	},
 	kit: {
 		adapter: netlify(),
