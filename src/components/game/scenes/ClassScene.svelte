b1c60fc6c441b933c363b1a652e0dd88fb29db0a
jason lim <50891910+Sxxov@users.noreply.github.com>
2022-06-23 01:40:11 +0800

    Resize class scene according to grid size

--- src/components/game/scenes/ClassScene.svelte
+++ src/components/game/scenes/ClassScene.svelte
@@ -3,15 +3,42 @@
 	import { fade } from '../../../core/transitioner';
 	import EntityRenderer from '../renderers/EntityRenderer.svelte';
 	import type { Game } from '../../../core/game/Game';
+	import { GameConstants } from '../../../core/game/GameConstants';
 
 	export let game: Game;
 
 	const { uiQueue: uiQueueW, round: roundW } = game;
 
+	let [ih, iw, oh, ow] = [0, 0, 0, 0];
+
 	$: ({ entityPool: entityPoolW } = $roundW!);
+
+	// sometimes inner* will glitch on chrome mobile, being wider than outer*
+	// get the smaller amongst them
+	$: [h, w] = [Math.min(ih, oh), Math.min(iw, ow)];
 </script>
 
-<div type="ClassScene" class="component" in:fade={{ duration: 1000 }}>
+<svelte:window
+	bind:innerHeight={ih}
+	bind:innerWidth={iw}
+	bind:outerHeight={oh}
+	bind:outerWidth={ow}
+/>
+
+<div
+	type="ClassScene"
+	class="component"
+	in:fade={{ duration: 1000 }}
+	style="
+		--height: {w > h
+		? h
+		: GameConstants.GRID_ROW_COUNT *
+		  (w / GameConstants.GRID_COLUMN_COUNT)}px;
+		--width: {w > h
+		? GameConstants.GRID_COLUMN_COUNT * (h / GameConstants.GRID_ROW_COUNT)
+		: w}px;
+	"
+>
 	{#if $uiQueueW[0]}
 		<UiRenderer
 			ui={$uiQueueW[0]}
@@ -28,7 +55,7 @@
 
 <style lang="postcss">
 	.component {
-		height: 100vmin;
-		width: 100vmin;
+		height: var(--height);
+		width: var(--width);
 	}
 </style>
