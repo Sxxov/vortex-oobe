da514208779af5bbd9ae5823c6457cd3fdde982f
jason lim <50891910+Sxxov@users.noreply.github.com>
2022-07-05 15:59:53 +0800

    Use correct sprites for inconspicuous machine

--- src/core/game/story/InconspicuousMachineEntity.ts
+++ src/core/game/story/InconspicuousMachineEntity.ts
@@ -1,18 +1,19 @@
-import inconspicuousMachine from '../../../assets/img/sprites/inconspicuous machine.png';
+import machineLone from '../../../assets/img/sprites/machine, lone.png';
+import machineWithCrumbs from '../../../assets/img/sprites/machine, w. crumbs.png';
 import { AlertComponent } from '../components/ui/AlertComponent';
 import { InteractivePropEntity } from '../entities/InteractivePropEntity';
 import { Sprite } from '../sprite/Sprite';
 
 export class InconspicuousMachineEntity extends InteractivePropEntity.for(
-	new Sprite(inconspicuousMachine),
-	new Sprite(inconspicuousMachine),
+	new Sprite(machineLone),
+	new Sprite(machineWithCrumbs),
 	[16, 8, 2, 2],
 	{
 		heading: 'Wack.',
 		message:
 			'Dr. Charles accidentally breaks the machine whilst cleaning it out. There goes his summer bonus.',
 		options: ['ouchy pockets...'],
-		sprite: new Sprite(inconspicuousMachine),
+		sprite: new Sprite(machineWithCrumbs),
 	},
 	[3, 2, 0],
 ) {
